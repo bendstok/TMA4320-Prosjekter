fb93a92d8565630b80efc98048a373f17ce55fa2
L-1006 <122864349+L-1006@users.noreply.github.com>
2023-01-25 11:46:12 +0100

    Basic tekst for 1a til 1d

--- Prosjekt Biofysikk/biofysikk.py
+++ Prosjekt Biofysikk/biofysikk.py
@@ -1,3 +1,33 @@
+'''Finne tumor med numeriske metoder
+
+Dette prosjektet angår å bruke numeriske metoder for å finne mulige tumor i kroppen. Rapporten går gjennom bit for bit hvordan en slik kode skal konstrueres, og går i detalj hvorfor koden blir konstruert slik. Prosjektet ender med en fungerende metode å finne tumor i pasienter.
+
+Vi må først forstå metoden før vi går løs på å kode en løsning. Metoden baserer seg på å måle vannmolekylenes bevegelse i kroppen. Siden mennesker har 70% vann, er dette en god tilnermelse. De måles i den virkelige verden ved å bruke deres magnetiske egenskaper, og deres måter å bevege seg i kroppen. Denne bevegenlsen er kalt for dispersjon. Dispersjon forteller hvordan vannmolekyler sprer seg over tid, ved at vannet sprer seg tregere i områder med høyere materialtetthet. Dette er nyttig, siden tumorer er karakterisert ved ukontrollert celledeling, som gir høyere materialtetthet. Til sist kan vi måle vannets dispersjon ved å se på hvordan vannets mangetiske egenskaper retter seg opp enten ved samme sted, eller andre steder. Dette betyr at vi kan bruke magnetiske målinger til å finne tumorer.
+
+Først går vi nermere inn på dispersjonslikningen:
+![bilde.png](attachment:bilde.png)
+![bilde-9.png](attachment:bilde-9.png)
+
+konstanten D er dispersjonskonstanten. jo lavere den er, jo tregere sprer molekyler seg. Matematikere har vist at dispersjon følger en gaussisk sannsynlighetsfordeling, og at forventningsverdien til posisjonen av et vannmolekyls posisjon, når det går ut i det uendelige, er startpunktet selv. Først skal vi vise at hvis σ^2 = at, så løser dette dispersjonslikningen ved riktig valg av a:
+
+![bilde-4.png](attachment:bilde-4.png) --> ![bilde-5.png](attachment:bilde-5.png) --> ![bilde-2.png](attachment:bilde-2.png)
+![bilde-6.png](attachment:bilde-6.png)
+![bilde-7.png](attachment:bilde-7.png)
+![bilde-8.png](attachment:bilde-8.png)
+
+Med a = 2D, løser likningen seg.
+
+Bilder er tatt fra wolfram alpha, og (Prosjektoppgavearket)
+
+
+Nå går vi løs på det numeriske.
+Vi starter med å konstruere en 1-dimensjonal virrevandrer, som beveger seg ett skritt enten til høyre eller til venstre, med lik sannsynlighet. Akkurat nå lager vi en enkel kode. vi forbedrer den senere.
+
+Med denne enkle modellen, tester vi den, med ulike sannsynligheter å gå til høyre eller venstre, for å sjekke om de er representative med den virkelige verden:
+
+Nå som vi har en virrevnadrer, lager vi flere av dem samtidig. vi lager den rask, og viktigst av alt, forståelig:
+
+
 # https://www.wolframalpha.com/input?i2d=true&i=D%5BDivide%5BPower%5Be%2C-%5C%2840%29Divide%5BPower%5Bx%2C2%5D%2C%5C%2840%292*a*t%5C%2841%29%5D%5C%2841%29+%5D%2Csqrt%5C%2840%292*pi*a*t%5C%2841%29%5D+%2C%7Bx%2C2%7D%5D
 # https://www.wolframalpha.com/input?i2d=true&i=D%5BDivide%5BPower%5Be%2C-%5C%2840%29Divide%5BPower%5Bx%2C2%5D%2C%5C%2840%292*a*t%5C%2841%29%5D%5C%2841%29+%5D%2Csqrt%5C%2840%292*pi*a*t%5C%2841%29%5D%2Ct%5D
 # a = 2 ^^
