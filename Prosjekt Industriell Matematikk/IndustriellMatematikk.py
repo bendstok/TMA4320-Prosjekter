59277666b09f0d4761bae94fed23bb928e48e728
L-1006 <122864349+L-1006@users.noreply.github.com>
2023-02-22 12:59:31 +0100

    Update IndustriellMatematikk.py
    1a og 1b, uten ferdig forklarende (men er litt selvforklarende og ¯\_(ツ)_/¯ )

---
+++ Prosjekt Industriell Matematikk/IndustriellMatematikk.py
@@ -0,0 +1,112 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+A1 = np.array([[1000, 1], [0, 1], [0, 0]])
+A2 = np.array([[1, 0, 0], [1, 0, 0], [0, 0, 1]])
+
+b1 = np.array([[2], [1], [0]])
+b2 = np.array([[0], [0], [1]])
+b3 = np.array([[0], [1], [0]])
+
+B = np.concatenate((b1, b2, b3), axis=1)
+
+def A1SVD(A1):
+    
+    """
+    a la text
+    """
+    
+    u, s, vt = np.linalg.svd(A1, full_matrices = False)
+    
+    print(f"u = {u}")
+    print(f"s = {s}")
+    print(f"vt = {vt}")
+    
+    fs = np.diag(s)
+
+    A = u @ fs @ vt
+    print(f"A1, u3 vekk = {A}")
+    
+    
+    """
+    Den første kolonnen i u er vekk, og likevel får vi rekonstruert A nøyaktig lik den fullt rekonstruerte A.
+    """
+    
+    u, s, vt = np.linalg.svd(A1, full_matrices = False)
+    
+    u[:,0] = 0
+    A = u @ fs @ vt
+    print(f"A1 u3 + u1 vekk = {A}")
+    
+    
+    """
+    Andre vekk gir et større feil, hvertfall for indeks [2][2], som er nå 10^-6
+    """
+    
+    u, s, vt = np.linalg.svd(A1, full_matrices = False)
+    
+    u[:,0] = 0
+    A = u @ fs @ vt
+    print(f"A1 u3 + u1 vekk = {A}")
+    
+    
+    """
+    Første vekk gir en matrise som ikke på noen måte har de samme verdiene der verdiene ikke er 0.
+    Det betyr at denne første kolonnen er viktigst for å rekonstruere matrisen.
+    """
+
+# print(A1SVD(A1))
+
+
+
+def A2SVD(A2):
+    
+    """
+    a la text
+    """
+    
+    u, s, vt = np.linalg.svd(A2, full_matrices = False)
+    
+    print(f"u = {u}")
+    print(f"s = {s}")
+    print(f"vt = {vt}")
+    
+    fs = np.diag(s)
+
+    A = u @ fs @ vt
+    print(f"A2 = {A}")
+    
+    u[:,2] = 0
+    A = u @ fs @ vt
+    print(f"A2, u3 vekk = {A}")
+    
+    """
+    Med å ta vekk u3, ser vi at vi får den nøyaktige samme rekonstruerte A som før
+    dermed 
+    
+    siden u# = [0, 1, 0], viser det at A2 ikke har noen tall ved andre kolonne.
+    Dermed kan vi fjerne denne delen ved å ta vekk u[:,3], og de korresponderende delene ved s, og vt,
+    og trygt få det samme resultatet: A2 = u_d s_d vt_d
+    """
+    
+# print(A2SVD(A2))
+
+def truncSVD(A, d):
+    
+    """
+    de glorious tex
+    """
+    
+    u, s, vt = np.linalg.svd(A, full_matrices = False)
+
+    # Velger de første d relevante vektorer og verdier
+    u = u[:, :d-1]
+    fs = np.diag(s[:d-1])
+    vt = vt[:d-1]
+    
+    W = u
+    H = fs @ vt
+    
+    return W, H
+    
+print(truncSVD(A2, 3))
